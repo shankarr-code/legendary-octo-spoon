65f1eae0f4ccca0d9eea772db2512f271a438242
Shankar Rajagopalan <you@example.com>
2021-04-24 18:12:57 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,147 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import {
+  HashRouter,
+  Switch,
+  Route
+} from "react-router-dom";
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import { css } from '@emotion/css';
+import Amplify, { API, Storage, Auth } from 'aws-amplify';
+//import { listPosts } from './graphql/queries';
 
-function App() {
+import Posts from './Posts';
+import Post from './Post';
+import Header from './Header';
+import CreatePost from './CreatePost';
+import Button from './Button';
+
+Amplify.configure({
+  // API gateway
+  API: {
+    endpoints: [
+      {
+        name: "myendpoint",
+        endpoint: "https://rcm4f2e5u7.execute-api.us-east-1.amazonaws.com/dev"
+      }
+    ]
+  }
+});
+
+function Router() {
+  /* create a couple of pieces of initial state */
+  const [showOverlay, updateOverlayVisibility] = useState(false);
+  const [posts, updatePosts] = useState([]);
+  const [user, setUser] = useState('')
+
+  /* fetch posts when component loads */
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  async function fetchPosts() {
+    const user = await Auth.currentAuthenticatedUser();
+    setUser(user);
+    const tenant = user.attributes['custom:tenant_id'];
+    console.log("user --> ", user);
+    const apiName = 'myendpoint';
+    const path = '/object';
+    const myInit = { // OPTIONAL
+      headers: {
+        "Authorization": `Bearer ${(await Auth.currentSession()).getIdToken().getJwtToken()}`,
+        "X-Amz-Security-Token": user.signInUserSession.idToken.jwtToken,
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json"
+      }, // OPTIONAL
+      queryStringParameters: {  // OPTIONAL
+        partition: 'db_nosql',
+      },
+      //'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'
+    };
+    console.log("header --> ", myInit);
+    // Get data from DynamoDB
+    let postData = await API.get(apiName, path, myInit);
+    console.log("Get Response ", postData);
+    console.log("postdata ", postData.result);
+    let postsArray = postData.result;
+    console.log('post array --> ', postsArray);
+
+    // Get signed url
+    const path2 = '/geturl';
+    /* map over the image keys in the posts array, get signed image URLs for each image */
+    postsArray = await Promise.all(postsArray.map(async post => {
+      //const sp_key = post.imageName.S.toString().split('/')[2];
+      const sp_key = post.imageName.S.toString();
+      console.log("keyname --> ", sp_key);
+      const myInit2 = {
+        headers: {
+          "Authorization": `Bearer ${(await Auth.currentSession()).getIdToken().getJwtToken()}`,
+          "X-Amz-Security-Token": user.signInUserSession.idToken.jwtToken,
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json"
+        },
+        queryStringParameters: {
+          action: 'get_url', key_name: `${sp_key}`
+        },
+        //'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'
+      };
+      const imageKey = await API.get(apiName, path2, myInit2);
+      //const imageKey = await Storage.get(post.imageName); // TODO API to get signed url
+      console.log('ImageKey --> ', imageKey);
+      //console.log('ImageKey url --> ', imageKey.result.toString().split(': \"')[1].replace('"}','') );
+      console.log('ImageKey url --> ', imageKey.result);
+      post.imageName = sp_key;
+      post.image = imageKey.result;
+      post.description = post.description.S.toString();
+      post.location = post.location.S.toString();
+      post.name = post.name.S.toString();
+      post.id = post.id.S.toString();
+      return post;
+    }));
+    /* update the posts array in the local state */
+    console.log("print postsArray --> ", postsArray);
+    setPostState(postsArray);
+  }
+  async function setPostState(postsArray) {
+    updatePosts(postsArray);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <HashRouter>
+        <div className={contentStyle}>
+          <Header
+            user={user} />
+          <hr className={dividerStyle} />
+          <Button title="New Post" onClick={() => updateOverlayVisibility(true)} />
+          <Switch>
+            <Route exact path="/" >
+              <Posts posts={posts} />
+            </Route>
+            <Route path="/post/:id" >
+              <Post
+                user={user} />
+            </Route>
+          </Switch>
+        </div>
+
+      </HashRouter>
+      { showOverlay && (
+        <CreatePost
+          updateOverlayVisibility={updateOverlayVisibility}
+          updatePosts={setPostState}
+          posts={posts}
+          user={user}
+        />
+      )}
+    </>
   );
 }
 
-export default App;
+const dividerStyle = css`
+  margin-top: 15px;
+`
+
+const contentStyle = css`
+  min-height: calc(100vh - 45px);
+  padding: 0px 40px;
+`
+export default withAuthenticator(Router, { includeGreetings: true });
